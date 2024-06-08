c48792baa1ef8980265c77724026138fab6f81a2
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-08 08:21:33 -0800

    first commit

--- PrintNumbersfromNto1.cpp
+++ PrintNumbersfromNto1.cpp
@@ -1,16 +1,37 @@
+// using linearly
+
+// #include<iostream>
+// using namespace std;
+
+// void func(int i,int n){
+//     if(i<1){
+//         return;
+//     }
+//     cout<<i<<endl;
+//     func(i-1,n);
+   
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     func(n,n);
+// }
+
+// using backtracking
+
+
 #include<iostream>
 using namespace std;
 
 void func(int i,int n){
-    if(i<1){
+    if(i>n){
         return;
     }
+     func(i+1,n);
     cout<<i<<endl;
-    func(i-1,n);
-   
 }
 int main(){
     int n;
     cin>>n;
-    func(n,n);
+    func(1,n);
 }
