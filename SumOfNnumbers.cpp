27d5f8b0d7f5f921fc33ee0df052a2235055290c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-08 08:27:35 -0800

    first commit

---
+++ SumOfNnumbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void add(int i, int sum){
+    if(i<1){
+        cout<<sum<<endl;
+        return;
+    }
+    add(i-1,sum+i);
+}
+int main(){
+    int n;
+    cin>>n;
+    add(n,0);
+}
