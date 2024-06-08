b9d50bb73865dd588c6093b1599b5d15b7baccab
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-08 10:33:04 -0800

    first commit

--- Factorial.cpp
+++ Factorial.cpp
@@ -15,3 +15,40 @@ int main(){
     cin>>n;
     cout<<fact(n);
 }
+
+
+
+
+
+// gfg question
+
+
+// A number N is called a factorial number if it is the factorial of a positive integer. For example, the first few factorial numbers are 1, 2, 6, 24, 120,
+// Given a number N, the task is to return the list/vector of the factorial numbers smaller than or equal to N.
+
+
+
+// class Solution
+// {
+// public:
+//     vector<long long> factorialNumbers(long long N)
+//     {
+        
+//       vector<long long>ans;
+//       long long fact = 1;
+    
+//       for(int i=1;i<=N;i++)
+//       {
+//           fact = fact*i;
+          
+//           if(fact<=N){
+//               ans.push_back(fact);
+//             }
+            
+//           else{
+//                   break;
+//               }
+//         }
+//       return ans;
+//     }
+// };
