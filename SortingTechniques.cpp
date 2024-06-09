144554a00a3dc8e7e3ca55c2eac549c122ef8632
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-09 01:39:47 -0800

    first commit

--- SortingTechniques.cpp
+++ SortingTechniques.cpp
@@ -36,6 +36,17 @@ void  bubble_sort(int arr[],int n){
     }
 }
 
+void insertion_sort(int arr[],int n){
+    for(int i=0;i<=n-1;i++){
+        int j =i;
+        while(j>0 && arr[j-1]>arr[j])
+        {
+            swap(arr[j],arr[j-1]);
+            j--;
+            cout<<"runs";
+        }
+    }
+}
 
 
 int main(){
