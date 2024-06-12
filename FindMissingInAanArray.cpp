a69f6bb443907faecf067a72d753464d4b0c995c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-12 05:48:59 -0800

    first commit

--- FindMissingInAanArray.cpp
+++ FindMissingInAanArray.cpp
@@ -6,17 +6,32 @@ using namespace std;
 
 int missingNumber(vector<int>&a, int N) {
 
+    // optimal1 --> using sum
+
     //Summation of first N numbers:
-    int sum = (N * (N + 1)) / 2;
+    // int sum = (N * (N + 1)) / 2;
 
-    //Summation of all array elements:
-    int s2 = 0;
+    // //Summation of all array elements:
+    // int s2 = 0;
+    // for (int i = 0; i < N - 1; i++) {
+    //     s2 += a[i];
+    // }
+
+    // int missingNum = sum - s2;
+    // return missingNum;
+
+
+    // OPTIMAL 2-> using xor
+
+     int xor1 = 0, xor2 = 0;
+
     for (int i = 0; i < N - 1; i++) {
-        s2 += a[i];
+        xor2 = xor2 ^ a[i]; // XOR of array elements
+        xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
     }
+    xor1 = xor1 ^ N; //XOR up to [1...N]
 
-    int missingNum = sum - s2;
-    return missingNum;
+    return (xor1 ^ xor2); // the missing number
 }
 
 int main()
