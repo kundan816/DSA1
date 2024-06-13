58e491870ef86d39cb15dc5385ef3593beb5f185
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-13 06:08:12 -0800

    first commit

--- MajorityElementInAnAraray.cpp
+++ MajorityElementInAnAraray.cpp
@@ -2,6 +2,44 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int majorityElement(vector<int> v) {
+
+//     //size of the given array:
+//     int n = v.size();
+
+//     for (int i = 0; i < n; i++) {
+//         //selected element is v[i]
+//         int cnt = 0;
+//         for (int j = 0; j < n; j++) {
+//             // counting the frequency of v[i]
+//             if (v[j] == v[i]) {
+//                 cnt++;
+//             }
+//         }
+
+//         // check if frquency is greater than n/2:
+//         if (cnt > (n / 2))
+//             return v[i];
+//     }
+
+//     return -1;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+//     int ans = majorityElement(arr);
+//     cout << "The majority element is: " << ans << endl;
+//     return 0;
+// }
+
+
+// Better
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,19 +48,19 @@ int majorityElement(vector<int> v) {
     //size of the given array:
     int n = v.size();
 
-    for (int i = 0; i < n; i++) {
-        //selected element is v[i]
-        int cnt = 0;
-        for (int j = 0; j < n; j++) {
-            // counting the frequency of v[i]
-            if (v[j] == v[i]) {
-                cnt++;
-            }
-        }
+    //declaring a map:
+    map<int, int> mpp;
 
-        // check if frquency is greater than n/2:
-        if (cnt > (n / 2))
-            return v[i];
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+    }
+
+    //searching for the majority element:
+    for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
     }
 
     return -1;
@@ -35,6 +73,8 @@ int main()
     cout << "The majority element is: " << ans << endl;
     return 0;
 }
+
+
 
 
 
