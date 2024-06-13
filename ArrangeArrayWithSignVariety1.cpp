3dd737786d77ac30ae1b02c3629864bb9cc8f105
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-13 11:47:49 -0800

    first commit

--- ArrangeArrayWithSignVariety1.cpp
+++ ArrangeArrayWithSignVariety1.cpp
@@ -3,47 +3,98 @@
 // brute -->
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+//   vector<int> RearrangebySign(vector<int>A, int n){
+    
+//   // Define 2 vectors, one for storing positive 
+//   // and other for negative elements of the array.
+//   vector<int> pos;
+//   vector<int> neg;
+  
+//   // Segregate the array into positives and negatives.
+//   for(int i=0;i<n;i++){
+      
+//       if(A[i]>0) 
+//       {
+//         pos.push_back(A[i]);
+//       }
+//       else{
+//          neg.push_back(A[i]);
+//       }
+//   }
+  
+//   // Positives on even indices, negatives on odd.
+//   for(int i=0;i<n/2;i++)
+//   {
+      
+//       A[2*i] = pos[i];
+//       A[2*i+1] = neg[i];
+//   }
+  
+  
+//   return A;
+    
+// }
+
+// int main() {
+    
+//   int n = 4;
+//   vector<int> A {1,2,-4,-5};
+
+//   vector<int> ans = RearrangebySign(A,n);
+  
+//   for (int i = 0; i < ans.size(); i++) {
+//     cout << ans[i] << " ";
+//   }
+
+//   return 0;
+// }
+
+
+// optimal  -->
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-  vector<int> RearrangebySign(vector<int>A, int n){
+  vector<int> RearrangebySign(vector<int>A){
     
-  // Define 2 vectors, one for storing positive 
-  // and other for negative elements of the array.
-  vector<int> pos;
-  vector<int> neg;
+  int n = A.size();
   
-  // Segregate the array into positives and negatives.
-  for(int i=0;i<n;i++){
+  // Define array for storing the ans separately.
+  vector<int> ans(n,0);
+  
+  // positive elements start from 0 and negative from 1.
+  int posIndex = 0, negIndex = 1;
+
+  for(int i = 0;i<n;i++){
       
-      if(A[i]>0) 
+      // Fill negative elements in odd indices and inc by 2.
+      if(A[i]<0)
       {
-        pos.push_back(A[i]);
+          ans[negIndex] = A[i];
+          negIndex+=2;
       }
-      else{
-         neg.push_back(A[i]);
-      }
-  }
-  
-  // Positives on even indices, negatives on odd.
-  for(int i=0;i<n/2;i++)
-  {
       
-      A[2*i] = pos[i];
-      A[2*i+1] = neg[i];
+      // Fill positive elements in even indices and inc by 2.
+      else{
+          ans[posIndex] = A[i];
+          posIndex+=2;
+      }
   }
   
-  
-  return A;
+  return ans;
     
 }
 
 int main() {
-    
-  int n = 4;
-  vector<int> A {1,2,-4,-5};
 
-  vector<int> ans = RearrangebySign(A,n);
+  
+  vector<int> A = {1,2,-4,-5};
+
+  vector<int> ans = RearrangebySign(A);
   
   for (int i = 0; i < ans.size(); i++) {
     cout << ans[i] << " ";
@@ -51,4 +102,5 @@ int main() {
 
   return 0;
 }
+
 
