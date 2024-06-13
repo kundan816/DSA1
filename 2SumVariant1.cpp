3a9a420ebdd001425655478b6339752fbc55760c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-13 05:03:47 -0800

    first commit

--- 2SumVariant1.cpp
+++ 2SumVariant1.cpp
@@ -1,18 +1,54 @@
+// Brute -->
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<int> twoSum(int n, vector<int> &arr, int target) {
+//     vector<int> ans;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] + arr[j] == target) {
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//                 return ans;
+//             }
+//         }
+//     }
+//     return { -1, -1};
+// }
+
+// int main()
+// {
+//     int n = 5;
+//     vector<int> arr = {2, 6, 5, 8, 11};
+//     int target = 14;
+//     vector<int> ans = twoSum(n, arr, target);
+//     cout << "This is the answer for variant 2: [" << ans[0] << ", "
+//          << ans[1] << "]" << endl;
+//     return 0;
+// }
+
+
+
+// optimal
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// brute
-
 vector<int> twoSum(int n, vector<int> &arr, int target) {
-    vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] + arr[j] == target) {
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
+    unordered_map<int, int> mpp;
+
+    for (int i = 0; i < n; i++) 
+    {
+        int num = arr[i];
+        int moreNeeded = target - num;
+
+        if (mpp.find(moreNeeded) != mpp.end()) 
+        {
+            return {mpp[moreNeeded], i};
         }
+        mpp[num] = i;
     }
     return { -1, -1};
 }
@@ -22,6 +58,7 @@ int main()
     int n = 5;
     vector<int> arr = {2, 6, 5, 8, 11};
     int target = 14;
+    
     vector<int> ans = twoSum(n, arr, target);
     cout << "This is the answer for variant 2: [" << ans[0] << ", "
          << ans[1] << "]" << endl;
