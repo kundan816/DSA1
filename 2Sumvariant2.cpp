c45cdae68e0debc51cee634a44c060f4c89481b0
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-13 05:00:28 -0800

    first commit

--- 2Sumvariant2.cpp
+++ 2Sumvariant2.cpp
@@ -30,18 +30,62 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// string twoSum(int n, vector<int> &arr, int target) {
+//     unordered_map<int, int> mpp;
+//     for (int i = 0; i < n; i++) {
+//         int num = arr[i];
+//         int moreNeeded = target - num;
+//         if (mpp.find(moreNeeded) != mpp.end()) {
+//             return "YES";
+//         }
+//         mpp[num] = i;
+//     }
+//     return "NO";
+// }
+
+// int main()
+// {
+//     int n = 5;
+//     vector<int> arr = {2, 6, 5, 8, 11};
+//     int target = 14;
+//     string ans = twoSum(n, arr, target);
+//     cout << "This is the answer for variant 1: " << ans << endl;
+//     return 0;
+// }
+
+
+
+
+// optimal
+
 #include <bits/stdc++.h>
 using namespace std;
 
 string twoSum(int n, vector<int> &arr, int target) {
-    unordered_map<int, int> mpp;
-    for (int i = 0; i < n; i++) {
-        int num = arr[i];
-        int moreNeeded = target - num;
-        if (mpp.find(moreNeeded) != mpp.end()) {
+
+    sort(arr.begin(), arr.end());
+
+    int left = 0, right = n - 1;
+    
+    while (left < right) 
+    {
+        int sum = arr[left] + arr[right];
+
+        if (sum == target) 
+        {
             return "YES";
         }
-        mpp[num] = i;
+        else if (sum < target) {
+                left++;
+        }
+            
+        else {
+            right--;
+        }
+           
     }
     return "NO";
 }
@@ -55,7 +99,4 @@ int main()
     cout << "This is the answer for variant 1: " << ans << endl;
     return 0;
 }
-
-
-
 
