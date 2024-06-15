ee4822eb7043c2cd4ab6622c87a89932d1794f43
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-15 04:36:32 -0800

    first commit

--- NumberOfSubArrayWithSumK.cpp
+++ NumberOfSubArrayWithSumK.cpp
@@ -1,6 +1,45 @@
 // brutte
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+//     int n = arr.size(); // size of the given array.
+//     int cnt = 0; // Number of subarrays:
+
+//     for (int i = 0 ; i < n; i++) { // starting index i
+//         for (int j = i; j < n; j++) { // ending index j
+
+//             // calculate the sum of subarray [i...j]
+//             int sum = 0;
+//             for (int K = i; K <= j; K++){
+//                 sum += arr[K];   
+//             }
+               
+
+//             // Increase the count if sum == k:
+//             if (sum == k)
+//                 cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {3, 1, 2, 4};
+//     int k = 6;
+//     int cnt = findAllSubarraysWithGivenSum(arr, k);
+//     cout << "The number of subarrays is: " << cnt << "\n";
+//     return 0;
+// }
+
+
+
+// Better
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,14 +48,11 @@ int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
     int cnt = 0; // Number of subarrays:
 
     for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
         for (int j = i; j < n; j++) { // ending index j
-
             // calculate the sum of subarray [i...j]
-            int sum = 0;
-            for (int K = i; K <= j; K++){
-                sum += arr[K];   
-            }
-               
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
 
             // Increase the count if sum == k:
             if (sum == k)
@@ -34,5 +70,4 @@ int main()
     cout << "The number of subarrays is: " << cnt << "\n";
     return 0;
 }
-
 
