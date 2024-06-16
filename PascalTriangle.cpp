0e43a5d468df3058d7d10624959d7d666f566f97
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-15 22:37:39 -0800

    first commit

--- PascalTriangle.cpp
+++ PascalTriangle.cpp
@@ -1,8 +1,7 @@
 // type1 -->
 
 
-// #include <bits/stdc++.h>
-// using namespace std;
+
 
 // int nCr(int n, int r) {
 //     long long res = 1;
@@ -21,29 +20,56 @@
 // type 2 -->
 
 
-    // Brute -->
+// Brute -->
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int nCr(int n, int r) {
+//     long long res = 1;
+
+//     // calculating nCr:
+//     for (int i = 0; i < r; i++) {
+//         res = res * (n - i);
+//         res = res / (i + 1);
+//     }
+//     return res;
+// }
+
+// void pascalTriangle(int n) {
+//     // printing the entire row n:
+//     for (int c = 1; c <= n; c++) {
+
+//         cout << nCr(n - 1, c - 1) << " ";
+//     }
+//     cout << "\n";
+// }
+
+// int main()
+// {
+//     int n = 5;
+//     pascalTriangle(n);
+//     return 0;
+// }
+
+// optimal  -->
+
+
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int nCr(int n, int r) {
-    long long res = 1;
-
-    // calculating nCr:
-    for (int i = 0; i < r; i++) {
-        res = res * (n - i);
-        res = res / (i + 1);
-    }
-    return res;
-}
-
 void pascalTriangle(int n) {
-    // printing the entire row n:
-    for (int c = 1; c <= n; c++) {
+    long long ans = 1;
+    cout << ans << " "; // printing 1st element
 
-        cout << nCr(n - 1, c - 1) << " ";
+    //Printing the rest of the part:
+    for (int i = 1; i < n; i++) {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
     }
-    cout << "\n";
+    cout << endl;
 }
 
 int main()
@@ -53,4 +79,6 @@ int main()
     return 0;
 }
 
-// optimaal
+
+
+
