8a0da349361b19ecf923cff08d407a9912a4a0ab
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-18 05:13:43 -0800

    first commit

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,26 +1,81 @@
 // Brute
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> triplet(int n, vector<int> &arr) {
+//     set<vector<int>> st;
+
+//     // check all possible triplets:
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 if (arr[i] + arr[j] + arr[k] == 0) {
+//                     vector<int> temp = {arr[i], arr[j], arr[k]};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+//                 }
+//             }
+//         }
+//     }
+
+//     //store the set elements in the answer:
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+
+// int main()
+// {
+//     vector<int> arr = { -1, 0, 1, 2, -1, -4};
+//     int n = arr.size();
+//     vector<vector<int>> ans = triplet(n, arr);
+//     for (auto it : ans) {
+//         cout << "[";
+//         for (auto i : it) {
+//             cout << i << " ";
+//         }
+//         cout << "] ";
+//     }
+//     cout << "\n";
+//     return 0;
+// }
+
+
+
+// better -->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<vector<int>> triplet(int n, vector<int> &arr) {
     set<vector<int>> st;
 
-    // check all possible triplets:
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (arr[i] + arr[j] + arr[k] == 0) {
-                    vector<int> temp = {arr[i], arr[j], arr[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
+    for (int i = 0; i < n; i++) 
+    {
+        set<int> hashset;
+
+        for (int j = i + 1; j < n; j++) 
+        {
+            //Calculate the 3rd element:
+            int third = -(arr[i] + arr[j]);
+
+            //Find the element in the set:
+            if (hashset.find(third) != hashset.end()) 
+            {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
             }
+            hashset.insert(arr[j]);
         }
     }
 
-    //store the set elements in the answer:
+    //store the set in the answer:
     vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
@@ -41,3 +96,4 @@ int main()
     cout << "\n";
     return 0;
 }
+
