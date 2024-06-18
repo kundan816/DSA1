9738f106aae9d130dce0e60040be76285e67398f
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-17 22:44:56 -0800

    first commit

--- MajorityElemet2.cpp
+++ MajorityElemet2.cpp
@@ -3,6 +3,57 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<int> majorityElement(vector<int> v) {
+//     int n = v.size(); //size of the array
+//     vector<int> ls; // list of answers
+
+//     for (int i = 0; i < n; i++) 
+//     {
+//         //selected element is v[i]:
+//         // Checking if v[i] is not already a part of the answer:
+//         if (ls.size() == 0 || ls[0] != v[i]) 
+//         {
+//             int cnt = 0;
+//             for (int j = 0; j < n; j++) {
+//                 // counting the frequency of v[i]
+//                 if (v[j] == v[i]) {
+//                     cnt++;
+//                 }
+//             }
+
+//             // check if frquency is greater than n/3:
+//             if (cnt > (n / 3))
+//                 ls.push_back(v[i]);
+//         }
+
+//         if (ls.size() == 2) 
+//             break;
+//     }
+
+//     return ls;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {11, 33, 33, 11, 33, 11};
+//     vector<int> ans = majorityElement(arr);
+//     cout << "The majority elements are: ";
+//     for (auto it : ans)
+//         cout << it << " ";
+//     cout << "\n";
+//     return 0;
+// }
+
+
+
+// Better
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,25 +61,21 @@ vector<int> majorityElement(vector<int> v) {
     int n = v.size(); //size of the array
     vector<int> ls; // list of answers
 
-    for (int i = 0; i < n; i++) 
-    {
-        //selected element is v[i]:
-        // Checking if v[i] is not already a part of the answer:
-        if (ls.size() == 0 || ls[0] != v[i]) 
+    //declaring a map:
+    map<int, int> mpp;
+
+    // least occurrence of the majority element:
+    int mini = int(n / 3) + 1;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+
+        //checking if v[i] is the majority element:
+        if (mpp[v[i]] == mini) 
         {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                // counting the frequency of v[i]
-                if (v[j] == v[i]) {
-                    cnt++;
-                }
-            }
-
-            // check if frquency is greater than n/3:
-            if (cnt > (n / 3))
-                ls.push_back(v[i]);
+            ls.push_back(v[i]);
         }
-
         if (ls.size() == 2) 
             break;
     }
@@ -46,6 +93,5 @@ int main()
     cout << "\n";
     return 0;
 }
-
 
 
