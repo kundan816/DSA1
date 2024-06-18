9bddf9fd407d0f832bbb09f8e2be1d3543f91a4e
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-18 07:45:18 -0800

    first commit

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -4,6 +4,60 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//     int n = nums.size(); //size of the array
+//     set<vector<int>> st;
+
+//     //checking all possible quadruplets:
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 for (int l = k + 1; l < n; l++) {
+//                     // taking bigger data type
+//                     // to avoid integer overflow:
+//                     long long sum = nums[i] + nums[j];
+//                     sum += nums[k];
+//                     sum += nums[l];
+
+//                     if (sum == target) {
+//                         vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+//                         sort(temp.begin(), temp.end());
+//                         st.insert(temp);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+//     int target = 9;
+//     vector<vector<int>> ans = fourSum(nums, target);
+//     cout << "The quadruplets are: \n";
+//     for (auto it : ans) {
+//         cout << "[";
+//         for (auto ele : it) {
+//             cout << ele << " ";
+//         }
+//         cout << "] ";
+//     }
+//     cout << "\n";
+//     return 0;
+// }
+
+
+// Better  -->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,20 +68,20 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     //checking all possible quadruplets:
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
+            set<long long> hashset;
             for (int k = j + 1; k < n; k++) {
-                for (int l = k + 1; l < n; l++) {
-                    // taking bigger data type
-                    // to avoid integer overflow:
-                    long long sum = nums[i] + nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-
-                    if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
+                // taking bigger data type
+                // to avoid integer overflow:
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+                if (hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
                 }
+                // put the kth element into the hashset:
+                hashset.insert(nums[k]);
             }
         }
     }
@@ -53,7 +107,7 @@ int main()
 }
 
 
-// Better  -->
+
 
 
 
