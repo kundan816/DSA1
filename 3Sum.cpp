c42e35f0784597ccaba7244842ac7d64a1a64cfa
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-18 05:17:27 -0800

    first commit

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -49,34 +49,109 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> triplet(int n, vector<int> &arr) {
+//     set<vector<int>> st;
+
+//     for (int i = 0; i < n; i++) 
+//     {
+//         set<int> hashset;
+
+//         for (int j = i + 1; j < n; j++) 
+//         {
+//             //Calculate the 3rd element:
+//             int third = -(arr[i] + arr[j]);
+
+//             //Find the element in the set:
+//             if (hashset.find(third) != hashset.end()) 
+//             {
+//                 vector<int> temp = {arr[i], arr[j], third};
+//                 sort(temp.begin(), temp.end());
+//                 st.insert(temp);
+//             }
+//             hashset.insert(arr[j]);
+//         }
+//     }
+
+//     //store the set in the answer:
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+
+// int main()
+// {
+//     vector<int> arr = { -1, 0, 1, 2, -1, -4};
+//     int n = arr.size();
+//     vector<vector<int>> ans = triplet(n, arr);
+//     for (auto it : ans) {
+//         cout << "[";
+//         for (auto i : it) {
+//             cout << i << " ";
+//         }
+//         cout << "] ";
+//     }
+//     cout << "\n";
+//     return 0;
+// }
+
+
+
+
+// Optimal  -->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<vector<int>> triplet(int n, vector<int> &arr) {
-    set<vector<int>> st;
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
 
     for (int i = 0; i < n; i++) 
     {
-        set<int> hashset;
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) 
+            continue;
 
-        for (int j = i + 1; j < n; j++) 
+        //moving 2 pointers:
+
+        int j = i + 1;
+        int k = n - 1;
+
+        while (j < k) 
         {
-            //Calculate the 3rd element:
-            int third = -(arr[i] + arr[j]);
+            int sum = arr[i] + arr[j] + arr[k];
 
-            //Find the element in the set:
-            if (hashset.find(third) != hashset.end()) 
-            {
-                vector<int> temp = {arr[i], arr[j], third};
-                sort(temp.begin(), temp.end());
-                st.insert(temp);
+            if (sum < 0) {
+                j++;
             }
-            hashset.insert(arr[j]);
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                ans.push_back(temp);
+
+
+                j++;
+                k--;
+                //skip the duplicates:
+                while (j < k && arr[j] == arr[j - 1]) 
+                {
+                    j++;
+                }
+                while (j < k && arr[k] == arr[k + 1]) 
+                {
+                    k--;
+                }
+            }
         }
     }
-
-    //store the set in the answer:
-    vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
 
