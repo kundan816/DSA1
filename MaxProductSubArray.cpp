bbbe7f08e68a4aea238ed48245e7d6b8c5db4542
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-21 04:53:07 -0800

    first commit

--- MaxProductSubArray.cpp
+++ MaxProductSubArray.cpp
@@ -2,23 +2,55 @@
 
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int maxProductSubArray(vector<int>& nums) {
+//     int result = INT_MIN;
+//     for(int i=0;i<nums.size();i++) 
+//     {
+//         for(int j=i;j<nums.size();j++) 
+//         {
+//             int prod = 1;
+//             for(int k=i;k<=j;k++) 
+//                 prod *= nums[k];
+//             result = max(result,prod);    
+//         }
+//     }
+//     return result;
+// }
+
+// int main() {
+//     vector<int> nums = {1,2,-3,0,-4,-5};
+//     cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+//     return 0;
+// }
+
+
+
+
+
+// Better -->
+
+
 
 
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxProductSubArray(vector<int>& nums) {
+int maxProductSubArray(vector<int>& nums) 
+{
     int result = INT_MIN;
     for(int i=0;i<nums.size();i++) 
     {
+        int p = 1;
         for(int j=i;j<nums.size();j++) 
         {
-            int prod = 1;
-            for(int k=i;k<=j;k++) 
-                prod *= nums[k];
-            result = max(result,prod);    
+         
+           p *= nums[j];
         }
+        result = max(result,p);
     }
     return result;
 }
@@ -28,7 +60,6 @@ int main() {
     cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
     return 0;
 }
-
 
 
 
