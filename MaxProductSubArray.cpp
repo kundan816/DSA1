365333b14b078c00ec13c42bd1663bc2a891a3ff
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-21 04:51:36 -0800

    first commit

---
+++ MaxProductSubArray.cpp
@@ -0,0 +1,34 @@
+// Brute -->
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProductSubArray(vector<int>& nums) {
+    int result = INT_MIN;
+    for(int i=0;i<nums.size();i++) 
+    {
+        for(int j=i;j<nums.size();j++) 
+        {
+            int prod = 1;
+            for(int k=i;k<=j;k++) 
+                prod *= nums[k];
+            result = max(result,prod);    
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,2,-3,0,-4,-5};
+    cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+    return 0;
+}
+
+
+
+
