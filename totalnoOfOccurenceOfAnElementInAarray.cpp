ebe17597d32ffabb6bcbbfb2e5a6191d4f437c0d
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-22 01:07:57 -0800

    first commit

--- totalnoOfOccurenceOfAnElementInAarray.cpp
+++ totalnoOfOccurenceOfAnElementInAarray.cpp
@@ -80,14 +80,20 @@ int lastOccurrence(vector<int> &arr, int n, int k) {
 
 pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
     int first = firstOccurrence(arr, n, k);
-    if (first == -1) return { -1, -1};
+
+    if (first == -1) 
+        return { -1, -1};
     int last = lastOccurrence(arr, n, k);
+
     return {first, last};
 }
 
 int count(vector<int>& arr, int n, int x) {
     pair<int, int> ans = firstAndLastPosition(arr, n, x);
-    if (ans.first == -1) return 0;
+
+    if (ans.first == -1) 
+        return 0;
+        
     return (ans.second - ans.first + 1);
 }
 
