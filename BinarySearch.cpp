3fe30f0a4bf70ef262b6574bd2dceffb8df9a96f
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-22 00:29:44 -0800

    first commit

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,41 +1,84 @@
 // Iterative Approach -->
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int binarySearch(vector<int>& nums, int target) {
+//     int n = nums.size(); //size of the array
+//     int low = 0;
+//     int high = n - 1;
+
+//     // Perform the steps:
+//     while (low <= high) 
+//     {
+//         int mid = (low + high) / 2;
+
+//         if (nums[mid] == target) 
+//             return mid;
+//         else if (target > nums[mid]) 
+//             low = mid + 1;
+//         else 
+//             high = mid - 1;
+//     }
+//     return -1;
+// }
+
+// int main()
+// {
+//     vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+//     int target = 6;
+
+//     int index = binarySearch(a, target);
+
+//     if (index == -1) 
+//         cout << "The target is not present." << endl;
+//     else 
+//         cout << "The target is at index: "<< index << endl;
+//     return 0;
+// }
+
+
+// Recursive Approach -->
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
-    int low = 0;
-    int high = n - 1;
+int binarySearch(vector<int>& nums, int low, int high, int target) {
 
-    // Perform the steps:
-    while (low <= high) 
-    {
-        int mid = (low + high) / 2;
+    if (low > high) 
+        return -1; //Base case.
 
-        if (nums[mid] == target) 
-            return mid;
-        else if (target > nums[mid]) 
-            low = mid + 1;
-        else 
-            high = mid - 1;
-    }
-    return -1;
+   
+    int mid = (low + high) / 2;
+
+    if (nums[mid] == target) 
+        return mid;
+
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+
+    else
+        return binarySearch(nums, low, mid - 1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
 }
 
 int main()
 {
     vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
     int target = 6;
-
-    int index = binarySearch(a, target);
-
-    if (index == -1) 
+    int ind = search(a, target);
+    if (ind == -1) 
         cout << "The target is not present." << endl;
     else 
-        cout << "The target is at index: "<< index << endl;
+        cout << "The target is at index: "<< ind << endl;
     return 0;
 }
+
 
 
