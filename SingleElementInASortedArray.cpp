4f957934f99ccd79113c2c85c3e16ebb7f176416
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-23 03:18:09 -0800

    first commit

--- SingleElementInASortedArray.cpp
+++ SingleElementInASortedArray.cpp
@@ -3,35 +3,63 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int singleNonDuplicate(vector<int>& arr) {
+//     int n = arr.size(); //size of the array.
+//     if (n == 1) 
+//         return arr[0];
+
+//     for (int i = 0; i < n; i++) 
+//     {
+
+//         //Check for first index:
+//         if (i == 0) {
+//             if (arr[i] != arr[i + 1])
+//                 return arr[i];
+//         }
+//         //Check for last index:
+//         else if (i == n - 1) {
+//             if (arr[i] != arr[i - 1])
+//                 return arr[i];
+//         }
+//         else {
+//             if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
+//                 return arr[i];
+//         }
+//     }
+
+//     // dummy return statement:
+//     return -1;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+//     int ans = singleNonDuplicate(arr);
+//     cout << "The single element is: " << ans << "\n";
+//     return 0;
+// }
+
+
+// Brute 2 -->
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int singleNonDuplicate(vector<int>& arr) {
     int n = arr.size(); //size of the array.
-    if (n == 1) 
-        return arr[0];
-
-    for (int i = 0; i < n; i++) 
-    {
-
-        //Check for first index:
-        if (i == 0) {
-            if (arr[i] != arr[i + 1])
-                return arr[i];
-        }
-        //Check for last index:
-        else if (i == n - 1) {
-            if (arr[i] != arr[i - 1])
-                return arr[i];
-        }
-        else {
-            if (arr[i] != arr[i - 1] && arr[i] != arr[i + 1])
-                return arr[i];
-        }
+    int ans = 0;
+    // XOR all the elements:
+    for (int i = 0; i < n; i++) {
+        ans = ans ^ arr[i];
     }
-
-    // dummy return statement:
-    return -1;
+    return ans;
 }
 
 int main()
