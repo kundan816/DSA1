178617eeb276a059ec2e68ab1495bc2cf2cef897
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-23 03:18:48 -0800

    first commit

--- SingleElementInASortedArray.cpp
+++ SingleElementInASortedArray.cpp
@@ -49,17 +49,71 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int singleNonDuplicate(vector<int>& arr) {
+//     int n = arr.size(); //size of the array.
+//     int ans = 0;
+//     // XOR all the elements:
+//     for (int i = 0; i < n; i++) {
+//         ans = ans ^ arr[i];
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+//     int ans = singleNonDuplicate(arr);
+//     cout << "The single element is: " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+// Optimal  -->
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int singleNonDuplicate(vector<int>& arr) {
     int n = arr.size(); //size of the array.
-    int ans = 0;
-    // XOR all the elements:
-    for (int i = 0; i < n; i++) {
-        ans = ans ^ arr[i];
+
+    //Edge cases:
+    if (n == 1) return arr[0];
+    if (arr[0] != arr[1]) return arr[0];
+    if (arr[n - 1] != arr[n - 2]) return arr[n - 1];
+
+    int low = 1, high = n - 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if arr[mid] is the single element:
+        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+            return arr[mid];
+        }
+
+        //we are in the left:
+        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1])
+                || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+            //eliminate the left half:
+            low = mid + 1;
+        }
+        //we are in the right:
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
     }
-    return ans;
+
+    // dummy return statement:
+    return -1;
 }
 
 int main()
