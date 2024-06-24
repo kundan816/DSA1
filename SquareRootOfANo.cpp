db4e5474502e05ef2a4d299aa3b6914d3d5790dd
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-24 04:45:14 -0800

    first commit

--- SquareRootOfANo.cpp
+++ SquareRootOfANo.cpp
@@ -3,20 +3,47 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int floorSqrt(int n) {
+//     int ans = 0;
+//     //linear search on the answer space:
+//     for (long long i = 1; i <= n; i++) {
+//         long long val = i * i;
+//         if (val <= n ) {
+//             ans = i;
+//         } else {
+//             break;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     int n = 28;
+//     int ans = floorSqrt(n);
+//     cout << "The floor of square root of " << n
+//          << " is: " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+
+// optimal 1 -->
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int floorSqrt(int n) {
-    int ans = 0;
-    //linear search on the answer space:
-    for (long long i = 1; i <= n; i++) {
-        long long val = i * i;
-        if (val <= n ) {
-            ans = i;
-        } else {
-            break;
-        }
-    }
+    int ans = sqrt(n);
     return ans;
 }
 
@@ -25,7 +52,52 @@ int main()
     int n = 28;
     int ans = floorSqrt(n);
     cout << "The floor of square root of " << n
-         << " is: " << ans << "\n";
+         << " is: " << ans;
     return 0;
 }
+
+
+
+
+
+
+// optimal 2-->
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int floorSqrt(int n) 
+// {
+//     int low = 1, high = n;
+
+   
+//     while (low <= high) {
+//         long long mid = (low + high) / 2;
+//         long long val = mid * mid;
+//         if (val <= (long long)(n)) {
+//             //eliminate the left half:
+//             low = mid + 1;
+//         }
+//         else {
+//             //eliminate the right half:
+//             high = mid - 1;
+//         }
+//     }
+//     return high;
+// }
+
+// int main()
+// {
+//     int n = 28;
+//     int ans = floorSqrt(n);
+//     cout << "The floor of square root of " << n
+//          << " is: " << ans << "\n";
+//     return 0;
+// }
+
+
 
