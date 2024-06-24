32f8ecbc1a78ae7b6b675a80ab6a406383eea3ff
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-24 04:27:59 -0800

    first commit

---
+++ SquareRootOfANo.cpp
@@ -0,0 +1,31 @@
+// brute -->
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int n) {
+    int ans = 0;
+    //linear search on the answer space:
+    for (long long i = 1; i <= n; i++) {
+        long long val = i * i;
+        if (val <= n ) {
+            ans = i;
+        } else {
+            break;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 28;
+    int ans = floorSqrt(n);
+    cout << "The floor of square root of " << n
+         << " is: " << ans << "\n";
+    return 0;
+}
+
