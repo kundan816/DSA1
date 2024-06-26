3a742bed70a6ef91fafb8036feed621d0b97c453
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-26 05:15:54 -0800

    first commit

--- KthMissingNumber.cpp
+++ KthMissingNumber.cpp
@@ -3,20 +3,57 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int missingK(vector < int > vec, int n, int k) {
+//     for (int i = 0; i < n; i++) 
+//     {
+//         if (vec[i] <= k)
+//              k++; 
+//         else 
+//             break;
+//     }
+//     return k;
+// }
+
+
+// int main()
+// {
+//     vector<int> vec = {4, 7, 9, 10};
+//     int n = 4, k = 4;
+//     int ans = missingK(vec, n, k);
+//     cout << "The missing number is: " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+// Optimal  --->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int missingK(vector < int > vec, int n, int k) {
-    for (int i = 0; i < n; i++) 
+    int low = 0, high = n - 1;
+    while (low <= high) 
     {
-        if (vec[i] <= k)
-             k++; 
-        else 
-            break;
+        int mid = (low + high) / 2;
+        int missing = vec[mid] - (mid + 1);
+        if (missing < k) 
+        {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
     }
-    return k;
+    return k + high + 1;
 }
-
 
 int main()
 {
