b8fcf6fffa2a95041d9b12076e352f7505a48520
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-26 05:14:47 -0800

    first commit

--- KthMissingNumber.cpp
+++ KthMissingNumber.cpp
@@ -7,9 +7,12 @@
 using namespace std;
 
 int missingK(vector < int > vec, int n, int k) {
-    for (int i = 0; i < n; i++) {
-        if (vec[i] <= k) k++; //shifting k
-        else break;
+    for (int i = 0; i < n; i++) 
+    {
+        if (vec[i] <= k)
+             k++; 
+        else 
+            break;
     }
     return k;
 }
