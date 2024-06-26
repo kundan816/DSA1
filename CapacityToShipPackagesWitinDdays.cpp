6f8b05b130dd04eabe0b8cc8e48fed544245d907
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-26 05:12:39 -0800

    first commit

--- CapacityToShipPackagesWitinDdays.cpp
+++ CapacityToShipPackagesWitinDdays.cpp
@@ -65,8 +65,10 @@ int findDays(vector<int> &weights, int cap) {
     int days = 1; //First day.
     int load = 0;
     int n = weights.size(); //size of array.
-    for (int i = 0; i < n; i++) {
-        if (load + weights[i] > cap) {
+    for (int i = 0; i < n; i++) 
+    {
+        if (load + weights[i] > cap) 
+        {
             days += 1; //move to next day
             load = weights[i]; //load the weight.
         }
@@ -82,10 +84,15 @@ int leastWeightCapacity(vector<int> &weights, int d) {
     //Find the maximum and the summation:
     int low = *max_element(weights.begin(), weights.end());
     int high = accumulate(weights.begin(), weights.end(), 0);
-    while (low <= high) {
+
+    while (low <= high) 
+    {
         int mid = (low + high) / 2;
+
         int numberOfDays = findDays(weights, mid);
-        if (numberOfDays <= d) {
+
+        if (numberOfDays <= d) 
+        {
             //eliminate right half
             high = mid - 1;
         }
