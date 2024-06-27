bc8666c633b466d6cc579e543fc962355ff6c1f2
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-27 05:50:01 -0800

    first commit

--- AggresiveCows.cpp
+++ AggresiveCows.cpp
@@ -3,6 +3,59 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool canWePlace(vector<int> &stalls, int dist, int cows) {
+//     int n = stalls.size(); //size of array
+//     int cntCows = 1; //no. of cows placed
+//     int last = stalls[0]; //position of last placed cow.
+//     for (int i = 1; i < n; i++) 
+//     {
+//         if (stalls[i] - last >= dist) 
+//         {
+//             cntCows++; //place next cow.
+//             last = stalls[i]; //update the last location.
+//         }
+//         if (cntCows >= cows) 
+//             return true;
+//     }
+//     return false;
+// }
+// int aggressiveCows(vector<int> &stalls, int k) {
+//     int n = stalls.size(); //size of array
+//     //sort the stalls[]:
+//     sort(stalls.begin(), stalls.end());
+
+//     int limit = stalls[n - 1] - stalls[0];
+//     for (int i = 1; i <= limit; i++) 
+//     {
+//         if (canWePlace(stalls, i, k) == false) 
+//         {
+//             return (i - 1);
+//         }
+//     }
+//     return limit;
+// }
+
+// int main()
+// {
+//     vector<int> stalls = {0, 3, 4, 7, 10, 9};
+//     int k = 4;
+//     int ans = aggressiveCows(stalls, k);
+//     cout << "The maximum possible minimum distance is: " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
+
+// Optimal  -->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,7 +70,7 @@ bool canWePlace(vector<int> &stalls, int dist, int cows) {
             cntCows++; //place next cow.
             last = stalls[i]; //update the last location.
         }
-        if (cntCows >= cows) 
+        if (cntCows >= cows)  //all cows gets placed 
             return true;
     }
     return false;
@@ -27,15 +80,19 @@ int aggressiveCows(vector<int> &stalls, int k) {
     //sort the stalls[]:
     sort(stalls.begin(), stalls.end());
 
-    int limit = stalls[n - 1] - stalls[0];
-    for (int i = 1; i <= limit; i++) 
+    int low = 1, high = stalls[n - 1] - stalls[0];
+    //apply binary search:
+    while (low <= high) 
     {
-        if (canWePlace(stalls, i, k) == false) 
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) 
         {
-            return (i - 1);
+            low = mid + 1;
         }
+        else 
+            high = mid - 1;
     }
-    return limit;
+    return high;
 }
 
 int main()
