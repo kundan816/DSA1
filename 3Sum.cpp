ba934b7d29d28cb78026b81551bf4c7e6ddcfdaa
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-28 03:38:42 -0800

    first commit

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -161,10 +161,13 @@ int main()
 {
     vector<int> arr = { -1, 0, 1, 2, -1, -4};
     int n = arr.size();
+    
     vector<vector<int>> ans = triplet(n, arr);
-    for (auto it : ans) {
+    for (auto it : ans) 
+    {
         cout << "[";
-        for (auto i : it) {
+        for (auto i : it) 
+        {
             cout << i << " ";
         }
         cout << "] ";
