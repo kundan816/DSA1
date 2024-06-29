0d53f1e9d35a20d0da6a7c0cab38fb639ff714ea
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-06-29 06:16:35 -0800

    first commit

--- ImplementUpperBound.cpp
+++ ImplementUpperBound.cpp
@@ -45,7 +45,8 @@ int upperBound(vector<int> &arr, int x, int n) {
             //look for smaller index on the left
             high = mid - 1;
         }
-        else {
+        else 
+        {
             low = mid + 1; // look on the right
         }
     }
@@ -57,7 +58,7 @@ int main()
     vector<int> arr = {3, 5, 8, 9, 15, 19};
     int n = 6, x = 9;
     int ind = upperBound(arr, x, n);
-    cout << "The upper bound is the index: " << ind << "\n";
+    cout << "The upper bound is the index : " << ind << "\n";
     return 0;
 }
 
