23403a63ca725765cba8876a6e456d46231d8063
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-01 08:59:28 -0800

    first commit

--- totalnoOfOccurenceOfAnElementInAarray.cpp
+++ totalnoOfOccurenceOfAnElementInAarray.cpp
@@ -39,16 +39,20 @@ int firstOccurrence(vector<int> &arr, int n, int k) {
 
     while (low <= high) {
         int mid = (low + high) / 2;
-        // maybe an answer
-        if (arr[mid] == k) {
+        // may be an answer
+        if (arr[mid] == k) 
+        {
             first = mid;
             //look for smaller index on the left
             high = mid - 1;
         }
-        else if (arr[mid] < k) {
+        else if (arr[mid] < k) 
+        {
             low = mid + 1; // look on the right
         }
-        else {
+        else 
+        {
+            
             high = mid - 1; // look on the left
         }
     }
