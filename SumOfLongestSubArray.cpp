6ea21a34f01edca8b789bc9b56584eb3fbf00f48
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-04 07:50:46 -0800

    first commit

--- SumOfLongestSubArray.cpp
+++ SumOfLongestSubArray.cpp
@@ -90,7 +90,8 @@ long long maxSubarraySum(int arr[], int n) {
     long long maxi = LONG_MIN; // maximum sum
     long long sum = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
 
         sum += arr[i];
 
