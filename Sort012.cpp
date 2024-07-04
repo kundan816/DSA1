c8e56e86431662ff22a5e9153246543af13227e9
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-04 07:50:27 -0800

    first commit

--- Sort012.cpp
+++ Sort012.cpp
@@ -57,7 +57,8 @@ void sortArray(vector<int>& arr, int n) {
 
     int low = 0, mid = 0, high = n - 1; // 3 pointers
 
-    while (mid <= high) {
+    while (mid <= high) 
+    {
         if (arr[mid] == 0) 
         {
             swap(arr[low], arr[mid]);
