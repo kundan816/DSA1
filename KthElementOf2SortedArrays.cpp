8d2cdc1c54cb15393cb84db971dd89f9e72e1099
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-08 02:02:46 -0800

    first commit

--- KthElementOf2SortedArrays.cpp
+++ KthElementOf2SortedArrays.cpp
@@ -1,31 +1,81 @@
 // Brute -->
 
 
-            #include <bits/stdc++.h>
-            using namespace std;
+            // #include <bits/stdc++.h>
+            // using namespace std;
             
-            int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
-                vector<int> arr3;
+            // int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+            //     vector<int> arr3;
             
-                //apply the merge step:
-                int i = 0, j = 0;
-                while (i < m && j < n) {
-                    if (a[i] < b[j]) arr3.push_back(a[i++]);
-                    else arr3.push_back(b[j++]);
-                }
+            //     //apply the merge step:
+            //     int i = 0, j = 0;
+            //     while (i < m && j < n) {
+            //         if (a[i] < b[j]) arr3.push_back(a[i++]);
+            //         else arr3.push_back(b[j++]);
+            //     }
             
-                //copy the left-out elements:
-                while (i < m) arr3.push_back(a[i++]);
-                while (j < n) arr3.push_back(b[j++]);
-                return arr3[k - 1];
-            }
+            //     //copy the left-out elements:
+            //     while (i < m) arr3.push_back(a[i++]);
+            //     while (j < n) arr3.push_back(b[j++]);
+            //     return arr3[k - 1];
+            // }
             
-            int main()
-            {
-                vector<int> a = {2, 3, 6, 7, 9};
-                vector<int> b = {1, 4, 8, 10};
-                cout << "The k-th element of two sorted array is: " <<
-                        kthElement(a, b, a.size(), b.size(), 5) << '\n';
-            }
+            // int main()
+            // {
+            //     vector<int> a = {2, 3, 6, 7, 9};
+            //     vector<int> b = {1, 4, 8, 10};
+            //     cout << "The k-th element of two sorted array is: " <<
+            //             kthElement(a, b, a.size(), b.size(), 5) << '\n';
+            // }
+        
+
+
+    // Better -->
+
+
+    
+#include <bits/stdc++.h>
+using namespace std;
+
+int kthElement(vector<int> &a, vector<int>& b, int m, int n, int k) {
+    int ele = -1;
+    int cnt = 0; //counter
+    //apply the merge step:
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (a[i] < b[j]) {
+            if (cnt == k - 1) ele = a[i];
+            cnt++;
+            i++;
+        }
+        else {
+            if (cnt == k - 1) ele = b[j];
+            cnt++;
+            j++;
+        }
+    }
+
+    //copy the left-out elements:
+    while (i < m) {
+        if (cnt == k - 1) ele = a[i];
+        cnt++;
+        i++;
+    }
+    while (j < n) {
+        if (cnt == k - 1) ele = b[j];
+        cnt++;
+        j++;
+    }
+    return ele;
+}
+
+int main()
+{
+    vector<int> a = {2, 3, 6, 7, 9};
+    vector<int> b = {1, 4, 8, 10};
+    cout << "The k-ht element of two sorted array is: " <<
+            kthElement(a, b, a.size(), b.size(), 5) << '\n';
+}
+        
         
         
