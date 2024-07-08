0cf5e8068e97ae93cb5af6f25d9c1aa899cd1af8
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-08 09:16:10 -0800

    first commit

--- SearchIn2-DMatrix2.cpp
+++ SearchIn2-DMatrix2.cpp
@@ -4,18 +4,62 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool searchElement(vector<vector<int>>& matrix, int target) {
+//     int n = matrix.size(), m = matrix[0].size();
+
+//     //traverse the matrix:
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//             if (matrix[i][j] == target)
+//                 return true;
+//         }
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19},
+//         {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24},
+//         {18, 21, 23, 26, 30}
+//     };
+//     searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+// }
+
+
+
+
+// Better -->
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-bool searchElement(vector<vector<int>>& matrix, int target) {
-    int n = matrix.size(), m = matrix[0].size();
+bool binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
 
-    //traverse the matrix:
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+bool searchElement(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == target)
-                return true;
-        }
+        bool flag =  binarySearch(matrix[i], target);
+        if (flag) return true;
     }
     return false;
 }
@@ -28,4 +72,5 @@ int main()
     };
     searchElement(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
 }
+
 
