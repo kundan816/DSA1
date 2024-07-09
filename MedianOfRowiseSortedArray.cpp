551fdb9bbee8121072b62b0247f17fdb2190fabb
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-09 00:38:31 -0800

    first commit

--- MedianOfRowiseSortedArray.cpp
+++ MedianOfRowiseSortedArray.cpp
@@ -1,23 +1,91 @@
 // brute -->
 
                                     
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int median(vector<vector<int>> &matrix, int m, int n) {
+//     vector<int> lst;
+
+//     //traverse the matrix and
+//     //copy the elements to list:
+//     for (int i = 0; i < m; i++) {
+//         for (int j = 0; j < n; j++) {
+//             lst.push_back(matrix[i][j]);
+//         }
+//     }
+
+//     //sort the list:
+//     sort(lst.begin(), lst.end());
+//     return lst[(m * n) / 2];
+// }
+
+// int main() {
+//     vector<vector<int>> matrix = {
+//         {1, 2, 3, 4, 5},
+//         {8, 9, 11, 12, 13},
+//         {21, 23, 25, 27, 29}
+//     };
+//     int m = matrix.size(), n = matrix[0].size();
+//     int ans = median(matrix, m, n);
+//     cout << "The median element is: " << ans << endl;
+//     return 0;
+// }
+                                    
+                                
+
+
+// optimal -->
+
+
+                                    
 #include<bits/stdc++.h>
 using namespace std;
 
-int median(vector<vector<int>> &matrix, int m, int n) {
-    vector<int> lst;
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
 
-    //traverse the matrix and
-    //copy the elements to list:
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            lst.push_back(matrix[i][j]);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
         }
     }
+    return ans;
+}
 
-    //sort the list:
-    sort(lst.begin(), lst.end());
-    return lst[(m * n) / 2];
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low = INT_MAX, high = INT_MIN;
+
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][n - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(matrix, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
 }
 
 int main() {
