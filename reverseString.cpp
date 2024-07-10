2f06a72dc26f93f35155ef8246a7be01d1121ec3
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-10 06:04:16 -0800

    first commit

---
+++ reverseString.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0;
+        int j = s.size() - 1 ;
+        while(i < j){
+            swap(s[i++],s[j--]);
+            // i++;
+            // j--;
+        }
+    }
+};
