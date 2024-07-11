a6dbd53c5ff761b1785387a0a5d5049a3d3bc016
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-11 09:32:14 -0800

    first commit

--- longestCommonPrefix.cpp
+++ longestCommonPrefix.cpp
@@ -5,19 +5,35 @@ using namespace std;
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
+        // Approach1 
 
-        for(int i = 0 ; i < strs[0].length(); i++)
-        {
-            for(int j = 0; j < strs.size() - 1 ; j++)
-            {
-                if(strs[j][i] != strs[j+1][i])
-                {
-                    return ans;
-                }
-            }
-            ans += strs[0][i] ;
+
+
+        // string ans = "";
+
+        // for(int i = 0 ; i < strs[0].length(); i++)
+        // {
+        //     for(int j = 0; j < strs.size() - 1 ; j++)
+        //     {
+        //         if(strs[j][i] != strs[j+1][i])
+        //         {
+        //             return ans;
+        //         }
+        //     }
+        //     ans += strs[0][i] ;
+        // }
+        // return ans ;
+
+
+        // approach2.
+        
+        string ans = "";
+        sort(strs.begin() , strs.end());
+        int i = 0;
+        while(i < strs[0].length() && strs[0][i] == strs[strs.size() - 1][i]){
+            ans += strs[0][i];
+            i++;
         }
-        return ans ;
+        return ans;
     }
 };
