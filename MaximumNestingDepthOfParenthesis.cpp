3866f0e78a5d73dd05f25bd3a709c72af12c46f4
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-11 12:07:40 -0800

    first commit

---
+++ MaximumNestingDepthOfParenthesis.cpp
@@ -0,0 +1,29 @@
+// using stack
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> st;
+        int result = 0;
+
+        for(char &ch : s)
+        {
+            if(ch == '(')
+            {
+                st.push(ch);
+            }
+            else if(ch == ')')
+            {
+                st.pop();
+            }
+            result = max(result , (int)st.size());
+        }
+        return result;
+
+
+
+    }
+};
