a26d10f5cba5bfa9046cf1605c2f9c8f1ddac425
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-17 02:17:24 -0800

    first commit

---
+++ ReverseaSinglyLinkedList.cpp
@@ -0,0 +1,94 @@
+// Better(iterative)
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    public:
+        Node(int data1 , Node* next1){
+            data =data1;
+            next = next1;
+        }
+    public:
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
+
+};
+
+Node* convertArrToLL(vector<int>& arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i =  1; i < arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+
+Node* reverseLinkedList(Node* head) {
+ 
+    Node* temp = head;  
+   
+    stack<int> st;     
+
+    while (temp != nullptr) {
+        st.push(temp->data); 
+        temp = temp->next;    
+    }
+   
+    temp = head; 
+
+    while (temp != nullptr) {
+        temp->data = st.top();  
+        st.pop();              
+        temp = temp->next;     
+    }
+    return head;  
+}
+
+
+
+int main() {
+    // Create a linked list with
+    // values 1, 3, 2, and 4
+    Node* head = new Node(1);
+    head->next = new Node(3);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(4);
+
+    // Print the original linked list
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    // Reverse the linked list
+    head = reverseLinkedList(head);
+
+    // Print the reversed linked list
+    cout << "Reversed Linked List: ";
+    printLinkedList(head);
+
+    return 0;
+}
+
