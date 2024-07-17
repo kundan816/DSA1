43f4c8faa9ad994b208fb224fd83bec481669c2c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-17 02:02:31 -0800

    first commit

--- ReaverseaDLL.cpp
+++ ReaverseaDLL.cpp
@@ -1,6 +1,120 @@
 // Brute -->
 
 
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+
+// class Node {
+// public:
+   
+//     int data;   
+//     Node* next;     
+//     Node* back;     
+
+//     Node(int data1, Node* next1, Node* back1) {
+//         data = data1;
+//         next = next1;
+//         back = back1;
+//     }
+
+
+//     Node(int data1) {
+//         data = data1;
+//         next = nullptr;
+//         back = nullptr;
+//     }
+// };
+
+
+// Node* convertArr2DLL(vector<int> arr) {
+    
+    
+//     Node* head = new Node(arr[0]);
+    
+//     Node* prev = head;            
+
+//     for (int i = 1; i < arr.size(); i++) {
+       
+//         Node* temp = new Node(arr[i], nullptr, prev);
+        
+//         prev->next = temp; 
+//         prev = temp;       
+//     }
+  
+//     return head;  
+// }
+
+
+// void print(Node* head) {
+//     while (head != nullptr) {
+    
+//         cout << head->data << " "; 
+//         head = head->next;         
+//     }
+// }
+
+ 
+
+// Node* reverseDLL(Node* head){
+//     // if head is empty or there is only one element
+//     // we can directly just return the head
+//     if(head==NULL || head->next == NULL){
+//         return head;
+//     }
+    
+//     // Initialise a stack st
+//     stack<int> st;
+    
+//     // Initialise the node pointer temp at head
+//     Node* temp = head;
+    
+//     // Traverse the doubly linked list via the temp pointer
+//     while(temp!=NULL){
+//         // insert the data of the current node into the stack
+//         st.push(temp->data);
+//         // traverse further
+//         temp = temp->next;
+//     }
+    
+//     // Reinitialise temp to head
+//     temp = head;
+    
+//     // Second iteration of the DLL to replace the values
+//     while(temp!=NULL){
+//         // Replace the value pointed via temp with
+//         // the value from the top of the stack
+//         temp->data = st.top();
+//         // Pop the value from the stack
+//         st.pop();
+//         // Traverse further
+//         temp = temp->next;
+//     }
+    
+   
+//     return head;
+
+// }
+
+
+// int main() {
+//     vector<int> arr = {12, 5, 8, 7, 4};
+//     Node* head = convertArr2DLL(arr);
+//     cout << endl << "Doubly Linked List Initially:  " << endl;
+//     print(head);
+//     cout << endl << "Doubly Linked List After Reversing " << endl;
+    
+//      // Insert a node with value 10 at the end
+//     head = reverseDLL(head);
+//     print(head);
+// }
+
+
+
+
+// Better -->
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -58,43 +172,43 @@ void print(Node* head) {
  
 
 Node* reverseDLL(Node* head){
-    // if head is empty or there is only one element
-    // we can directly just return the head
-    if(head==NULL || head->next == NULL){
-        return head;
+   // Check if the list is empty
+    // or has only one node
+    if (head == NULL || head->next == NULL) {
+        // No change is needed;
+        // return the current head
+        return head; 
     }
     
-    // Initialise a stack st
-    stack<int> st;
+     // Initialize a pointer to
+     // the previous node
+    Node* prev = NULL;  
     
-    // Initialise the node pointer temp at head
-    Node* temp = head;
-    
-    // Traverse the doubly linked list via the temp pointer
-    while(temp!=NULL){
-        // insert the data of the current node into the stack
-        st.push(temp->data);
-        // traverse further
-        temp = temp->next;
-    }
-    
-    // Reinitialise temp to head
-    temp = head;
-    
-    // Second iteration of the DLL to replace the values
-    while(temp!=NULL){
-        // Replace the value pointed via temp with
-        // the value from the top of the stack
-        temp->data = st.top();
-        // Pop the value from the stack
-        st.pop();
-        // Traverse further
-        temp = temp->next;
-    }
-    
-   
-    return head;
+    // Initialize a pointer to
+    // the current node
+    Node* current = head;   
 
+    // Traverse the linked list
+    while (current != NULL) {
+        // Store a reference to
+        // the previous node
+        prev = current->back; 
+        
+        // Swap the previous and
+        // next pointers
+        current->back = current->next; 
+        
+        // This step reverses the links
+        current->next = prev;          
+        
+        // Move to the next node
+        // in the original list
+        current = current->back; 
+    }
+    
+    // The final node in the original list
+    // becomes the new head after reversal
+    return prev->back;
 }
 
 
