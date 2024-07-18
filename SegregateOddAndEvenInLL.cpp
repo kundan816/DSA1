3e2b6b79eef50e0ef0a95a0809e7fa3ab7d7a71d
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-18 00:42:05 -0800

    first commit

--- SegregateOddAndEvenInLL.cpp
+++ SegregateOddAndEvenInLL.cpp
@@ -46,3 +46,34 @@
         
 //     return head;
 //  }
+
+
+
+// Optimal
+
+    // ListNode* oddEvenList(ListNode* head) {
+
+    //     if (head == NULL || head->next == NULL)
+    //         return head; 
+
+    //     ListNode* odd = head; 
+    //     ListNode* even = head->next; 
+
+    //     ListNode* evenHead = even; 
+
+    //     while (even != NULL && even->next != NULL)  //as even is always after odd , so it is in last
+    //     {
+           
+    //         odd->next = odd->next->next;
+    //         even->next = even->next->next;
+
+    //         odd = odd->next;
+    //         even = even->next;
+
+    //     }
+
+    //     //link last odd pointer to first even pointer  
+    //     odd->next = evenHead;
+
+    //     return head; 
+    // }
