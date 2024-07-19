76e0d9cdf92e27599f466fa54ee695c17389c7c8
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-19 06:49:18 -0800

    first commit

--- SortList.cpp
+++ SortList.cpp
@@ -1,6 +1,113 @@
 // Brute -->
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node{
+//     public:
+//         int data;
+//         Node* next;
+//     public:
+//         Node(int data1 , Node* next1){
+//             data =data1;
+//             next = next1;
+//         }
+//     public:
+//         Node(int data1){
+//             data = data1;
+//             next = nullptr;
+//         }
+
+// };
+
+// Node* convertArrToLL(vector<int>& arr)
+// {
+//     Node* head = new Node(arr[0]);
+//     Node* mover = head;
+
+//     for(int i =  1; i < arr.size(); i++)
+//     {
+//         Node* temp = new Node(arr[i]);
+//         mover->next = temp;
+//         mover = temp;
+//     }
+//     return head;
+// }
+
+// void printLinkedList(Node* head) {
+//     Node* temp = head;
+//     while (temp != nullptr) {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     }
+//     cout << endl;
+// }
+
+// Node* sortLL(Node* head){
+//     // Create a vector to
+//     // store node values
+//     vector<int> arr;
+    
+//     // Temporary pointer to
+//     // traverse the linked list
+//     Node* temp = head;
+    
+//     // Traverse the linked list and
+//     // store node values in the vector
+//     while(temp!=NULL){
+//         arr.push_back(temp->data);
+//         temp = temp->next;
+//     }
+    
+//     // Sort the array
+//     // containing node values
+//     sort(arr.begin(), arr.end());
+    
+//     // Reassign sorted values to
+//     // the linked list nodes
+//     temp = head;
+//     for(int i = 0; i < arr.size(); i++){
+//         // Update the node's data
+//         // with the sorted values
+//         temp->data = arr[i]; 
+//         // Move to the next node
+//         temp = temp->next; 
+//     }
+    
+//     // Return the head of the
+//     // sorted linked list
+//     return head; 
+// }
+
+
+
+// int main() {
+//     // Linked List: 3 2 5 4 1
+//     Node* head = new Node(3);
+//     head->next = new Node(2);
+//     head->next->next = new Node(5);
+//     head->next->next->next = new Node(4);
+//     head->next->next->next->next = new Node(1);
+
+//     cout << "Original Linked List: ";
+//     printLinkedList(head);
+
+//     // Sort the linked list
+//     head = sortLL(head);
+
+//     cout << "Sorted Linked List: ";
+//     printLinkedList(head);
+
+//     return 0;
+// }
+
+
+
+
+// Optimal -->
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -44,43 +151,89 @@ void printLinkedList(Node* head) {
     cout << endl;
 }
 
-Node* sortLL(Node* head){
-    // Create a vector to
-    // store node values
-    vector<int> arr;
-    
-    // Temporary pointer to
-    // traverse the linked list
-    Node* temp = head;
-    
-    // Traverse the linked list and
-    // store node values in the vector
-    while(temp!=NULL){
-        arr.push_back(temp->data);
-        temp = temp->next;
-    }
-    
-    // Sort the array
-    // containing node values
-    sort(arr.begin(), arr.end());
-    
-    // Reassign sorted values to
-    // the linked list nodes
-    temp = head;
-    for(int i = 0; i < arr.size(); i++){
-        // Update the node's data
-        // with the sorted values
-        temp->data = arr[i]; 
-        // Move to the next node
+
+
+// Function to merge two sorted linked lists
+Node* mergeTwoSortedLinkedLists(Node* list1, Node* list2) {
+    // Create a dummy node to serve
+    // as the head of the merged list
+    Node* dummyNode = new Node(-1);
+    Node* temp = dummyNode;
+
+    // Traverse both lists simultaneously
+    while (list1 != nullptr && list2 != nullptr) {
+        // Compare elements of both lists and
+        // link the smaller node to the merged list
+        if (list1->data <= list2->data) {
+            temp->next = list1;
+            list1 = list1->next;
+        } else {
+            temp->next = list2;
+            list2 = list2->next;
+        }
+        // Move the temporary pointer
+        // to the next node
         temp = temp->next; 
     }
-    
-    // Return the head of the
-    // sorted linked list
-    return head; 
+
+    // If any list still has remaining
+    // elements, append them to the merged list
+    if (list1 != nullptr) {
+        temp->next = list1;
+    } else {
+        temp->next = list2;
+    }
+    // Return the merged list starting 
+    // from the next of the dummy node
+    return dummyNode->next;
 }
 
+// Function to find the middle of a linked list
+Node* findMiddle(Node* head){
+    // If the list is empty or has only one node
+    // the middle is the head itself
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
 
+    // Initializing slow and fast pointers
+    Node* slow = head;
+    Node* fast = head->next;
+
+    // Move the fast pointer twice as fast as the slow pointer
+    // When the fast pointer reaches the end, the slow pointer
+    // will be at the middle
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+// Function to perform merge sort on a linked list
+Node* sortLL(Node* head){
+    // Base case: if the list is empty or has only one node
+    // it is already sorted, so return the head
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+    
+    // Find the middle of the list using the findMiddle function
+    Node* middle = findMiddle(head);
+    
+    // Divide the list into two halves
+    Node* right = middle->next;
+    middle->next = nullptr;
+    Node* left = head;
+    
+    // Recursively sort the left and right halves
+    left = sortLL(left);
+    right = sortLL(right);
+    
+    // Merge the sorted halves using the mergeTwoSortedLinkedLists function
+    return mergeTwoSortedLinkedLists(left, right);
+}
 
 int main() {
     // Linked List: 3 2 5 4 1
@@ -99,5 +252,5 @@ int main() {
     cout << "Sorted Linked List: ";
     printLinkedList(head);
 
-    return 0;
+    
 }
