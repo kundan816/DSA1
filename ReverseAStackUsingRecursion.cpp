db80f28c24731ae53fdcdbb465d9eca1b543b21f
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-25 23:23:26 -0800

    new commit

--- ReverseAStackUsingRecursion.cpp
+++ ReverseAStackUsingRecursion.cpp
@@ -34,3 +34,39 @@
     
 // }
 
+
+
+
+
+
+
+// OPTIMAL
+
+//     void insertAtBottom(stack<int> &st, int element)
+//  {
+//     if (st.empty())
+//     {
+//         st.push(element);
+//         return;
+//     }
+    
+//     int top = st.top();
+//     st.pop();
+    
+//     insertAtBottom(st, element);
+    
+//     st.push(top);
+//  }
+ 
+// void Reverse(stack<int> &St)
+// {
+//     if (St.empty())
+//         return;
+        
+//     int top = St.top();
+//     St.pop();
+    
+//     Reverse(St);
+    
+//     insertAtBottom(St, top);
+// }
