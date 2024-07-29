88d38bba3b9fdbba7d4af122a477771cf2a86d0e
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-07-29 07:19:06 -0800

    new commit

--- DetectACycleInALinkedList.cpp
+++ DetectACycleInALinkedList.cpp
@@ -171,14 +171,16 @@ bool detectLoop(Node* head) {
 
     // Step 2: Traverse the linked list with
     // the slow and fast pointers
-    while (fast != nullptr && fast->next != nullptr) {
+    while (fast != nullptr && fast->next != nullptr) 
+    {
         // Move slow one step
         slow = slow->next;
         // Move fast two steps
         fast = fast->next->next;
 
         // Check if slow and fast pointers meet
-        if (slow == fast) {
+        if (slow == fast) 
+        {
             return true;  // Loop detected
         }
     }
