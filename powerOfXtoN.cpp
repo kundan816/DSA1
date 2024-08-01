d81d8b61e0775f3fb4e1169df7d3f66d6c7e4c4c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-08-01 12:58:42 -0800

    new commit

--- powerOfXtoN.cpp
+++ powerOfXtoN.cpp
@@ -27,6 +27,7 @@ double myPow(double x, int n) {
   double ans = 1.0;
   long long nn = n;
   if (nn < 0) nn = -1 * nn;
+
   while (nn) 
   {
     if (nn % 2) 
@@ -41,7 +42,9 @@ double myPow(double x, int n) {
     }
   }
   if (n < 0) 
+  {
     ans = (double)(1.0) / (double)(ans);
+  }
   return ans;
 }
 
