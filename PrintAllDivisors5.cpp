76cc63794864b03d7e0c0eab53bbcb464f7f7fb4
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-08-04 12:08:27 -0800

    new commit

--- PrintAllDivisors5.cpp
+++ PrintAllDivisors5.cpp
@@ -7,8 +7,10 @@ int main() {
 	int n;
 	cin>>n;
 
-    for(int i=1; i<=n; i++){
-        if(n%i == 0){
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i == 0)
+        {
             cout<<i<<" ";
         }
     }
