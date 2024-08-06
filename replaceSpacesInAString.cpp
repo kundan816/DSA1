b36b363cacba2524b4d53e6ef8b823bb7987759c
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2024-08-06 07:59:14 -0800

    new commit

--- replaceSpacesInAString.cpp
+++ replaceSpacesInAString.cpp
@@ -4,8 +4,10 @@ using namespace std;
 string replaceSpaces(string &str){
 	
 	string temp = "";
-	for(int i = 0; i < str.length() ; i++){
-		if(str[i] == ' '){
+	for(int i = 0; i < str.length() ; i++)
+	{
+		if(str[i] == ' ')
+		{
 			temp.push_back('@');
 			temp.push_back('4');
 			temp.push_back('0');
